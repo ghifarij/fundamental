19745e8c82695c6d8e9919b90e68cc88ed4da9c2
ghifarij <afga.ghifari@gmail.com>
2024-10-14 10:50:58 +0700

    Initial commit

--- map.ts
+++
@@ -1,8 +0,0 @@
-const myMap = new Map();
-
-myMap.set("David", "001");
-myMap.set("Georgia", "002");
-
-console.log(myMap);
-console.log(myMap.get("David"));
-console.log(`size: ${myMap.size}`);
---
+++ split.ts
@@ -0,0 +1,9 @@
+/*
+    input = "helloWorld"
+    output = ["hello", "World"]
+
+    input = "johnDoeMiller"
+    output = ["john", "Doe", "Miller"]
+*/
+
+function split(str: string) {}
